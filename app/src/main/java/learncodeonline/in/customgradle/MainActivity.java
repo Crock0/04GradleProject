50c4f0c6d927f04b692526d4c044023c9de0171c
suvratam <suvratampatra3@gmail.com>
2019-01-21 18:33:28 +0530

    Toasty messages

---
+++ app/src/main/java/learncodeonline/in/customgradle/MainActivity.java
@@ -0,0 +1,40 @@
+package learncodeonline.in.customgradle;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import es.dmoral.toasty.Toasty;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view){
+        switch (view.getId()){
+            case R.id.errorButton:
+                Toasty.error(this,"Login failed",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.successButton:
+                Toasty.success(this,"Login Success",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.infoButton:
+                Toasty.info(this,"Login detail",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.warningButton:
+                Toasty.warning(this,"OppsS!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageButton:
+                Toasty.normal(this,"Uploaded",Toast.LENGTH_SHORT,ContextCompat.getDrawable(this,R.drawable.ic_file_upload)).show();
+                break;
+        }
+
+
+    }
+}
